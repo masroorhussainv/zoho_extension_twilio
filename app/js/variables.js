4325614e7ff54b69a0935ee30189dfb8226fba39
Masroor Hussain <masroorh7@gmail.com>
2023-08-07 09:29:57 +0500

    Added dotenv package and removed node_modules folder from git repo

---
+++ app/js/variables.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+export const VARIABLES = {
+  twilioAccountSid: process.env.TWILIO_ACCOUNT_SID,
+  twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
+  twilioPhoneNumber: process.env.TWILIO_PHONE_NUMBER,
+  backendAuthToken: process.env.BACKEND_AUTH_TOKEN
+}
